3862310980c0617883d228ccbc7300461dbcc8bb
Rakesh salian <>
2021-10-10 14:37:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/stepcounter/StepListener.java
@@ -0,0 +1,8 @@
+package com.example.stepcounter;
+
+// Will listen to step alerts
+public interface StepListener {
+
+    public void step(long timeNs);
+
+}
---
+++ app/src/main/java/com/example/stepcounter/Track.java
@@ -0,0 +1,6 @@
+package com.example.stepcounter;
+
+import android.app.Activity;
+
+public class Track extends Activity {
+}
